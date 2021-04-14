825a277f4024e9a35466dd911f50c3793077af7d
Paweł <zpawel.zieba@gmail.com>
2021-04-14 17:43:11 +0200

    Initial commit

---
+++ source/main.py
@@ -0,0 +1,23 @@
+from hx711 import hx711
+from hx import HX711
+from time import sleep
+from machine import freq
+
+freq(80000000)
+tenso = hx711(5, 4)
+tenso.calibrate()
+
+while True:
+    # wynik = tenso.read_filtered(5, 4)
+    # print("")
+    # print("")
+    # print(wynik)
+    # print("g:" + str(wynik/420))
+    print(tenso.get_weight_g())
+    print(tenso.get_weight_kg())
+
+    #sleep(5)
+
+
+
+
